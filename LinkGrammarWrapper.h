c2a3eafff9fd3f9d83bf6562c523c8fabfcfecf6
Garima Joshi <garimajoshi3@yahoo.in>
2013-01-26 06:46:24 -0800

    Create LinkGrammarWrapper.h

---
+++ LinkGrammarWrapper.h
@@ -0,0 +1,47 @@
+#ifndef LINKGRAMMARWRAPPER_H
+#define LINKGRAMMARWRAPPER_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <link-grammar/link-includes.h>
+
+#DEFINE DEFAULT_LANGUAGE "en"
+
+class QString;
+class QMap;
+
+class LinkGrammarWrapper
+{
+public:
+	LinkGrammarWrapper();
+	~LinkGrammarWrapper();
+	void cleanUp();
+	bool parseSentence(QString givenText);
+	int getMaxTimeToParseInNumberOfSeconds();
+	void setMaxTimeToParseInNumberOfSeconds(int numberOfSeconds);
+	int getMaxMemoryInMegaBytes;
+	void setMaxMemoryInMegaBytes(int memoryInBytes);
+	int getDisjunctCount;
+	void setDisjunctCount(int count);
+	QString getLanguage();
+	void setLanguage(QString language);
+	void addLanguageToSetOfSupportedLanguagesWitDictionaryPath(QString language, QString dictionaryPath)
+private:
+	bool isLanguageAvailable(QString);
+	Dictionary      m_Dict;
+	Parse_Options   m_Opts;
+	int m_maxTimeToParseInNumberOfSeconds;
+	int m_maxMemoryInMegaBytes;
+	int m_disjunctCount;
+	QString m_languagePreference;
+	QMap<QString, QString> m_listOfAvailableLanguages;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LINKGRAMMARWRAPPER_H
