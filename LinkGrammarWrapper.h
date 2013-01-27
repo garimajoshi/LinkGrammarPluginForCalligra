a4a3fde1c18e3743b1a86428192e45ca80f86b07
Garima Joshi <garimajoshi3@yahoo.in>
2013-01-27 16:01:43 +0530

    Update LinkGrammarWrapper.h

--- LinkGrammarWrapper.h
+++ LinkGrammarWrapper.h
@@ -1,26 +1,25 @@
 #ifndef LINKGRAMMARWRAPPER_H
 #define LINKGRAMMARWRAPPER_H
 
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <link-grammar/link-includes.h>
-
-#DEFINE DEFAULT_LANGUAGE "en"
-
 class QString;
 class QMap;
+
+#define DEFAULT_LANGUAGE "en"
+
+extern "C"
+{
+#include <link-grammar/link-includes.h>
+}
 
 class LinkGrammarWrapper
 {
 public:
-	LinkGrammarWrapper();
-	~LinkGrammarWrapper();
-	void cleanUp();
-	bool parseSentence(QString givenText);
-	bool isLanguageAvailable(QString language);
+    LinkGrammarWrapper();
+    LinkGrammarWrapper(Dictionary dict, Parse_Options opts);
+    ~LinkGrammarWrapper();
+    bool parseSentence(const QString text) const;
+    void cleanUp();
+    bool isLanguageAvailable(QString language);
 	int getMaxTimeToParseInNumberOfSeconds();
 	void setMaxTimeToParseInNumberOfSeconds(int numberOfSeconds);
 	int getMaxMemoryInMegaBytes;
@@ -29,19 +28,19 @@ public:
 	void setDisjunctCount(int count);
 	QString getLanguage();
 	void setLanguage(QString language);
-	void addLanguageToSetOfSupportedLanguagesWithDictionaryPath(QString language, QString dictionaryPath = 0)
+	void addLanguageToSetOfSupportedLanguages(QString language, QString dictionaryPath = 0);
+
 private:
-	Dictionary      m_Dict;
-	Parse_Options   m_Opts;
-	int m_maxTimeToParseInNumberOfSeconds;
-	int m_maxMemoryInMegaBytes;
+    void cleanUpDictionary();
+    void cleanUpParseOptions();
+    Dictionary m_dict;
+    Parse_Options m_opts;
+    int m_maxTimeToParseInNumberOfSeconds;
+    int m_maxMemoryInMegaBytes;
 	int m_disjunctCount;
 	QString m_languagePreference;
+    QString m_currentDictionaryLanguage;
 	QMap<QString, QString> m_listOfAvailableLanguages;
-}
-
-#ifdef __cplusplus
-}
-#endif
+};
 
 #endif // LINKGRAMMARWRAPPER_H
