71ea0375b91e16fd445a9d3729ee8385f74b2e08
Garima Joshi <garimajoshi3@yahoo.in>
2013-02-13 00:04:14 -0800

    Update LinkGrammarWrapper.h

--- LinkGrammarWrapper.h
+++ LinkGrammarWrapper.h
@@ -9,36 +9,36 @@ extern "C"
 {
 #include <link-grammar/link-includes.h>
 #endif
-
-class LinkGrammarWrapper
-{
-public:
-    LinkGrammarWrapper();
-    LinkGrammarWrapper(Dictionary dict, Parse_Options opts);
-    ~LinkGrammarWrapper();
-    bool parseSentence(const QString text) const;
-    void cleanUp();
-    bool isLanguageAvailable(QString language);
-    int getMaxTimeToParseInNumberOfSeconds();
-    void setMaxTimeToParseInNumberOfSeconds(int numberOfSeconds);
-    int getDisjunctCount;
-    void setDisjunctCount(int count);
-    QString getLanguage();
-    void setLanguage(QString language);
-    void addLanguageToSetOfSupportedLanguages(QString language, QString dictionaryPath = QString());
-
-private:
-    void cleanUpDictionary();
-    void cleanUpParseOptions();
-    Dictionary m_Dict;
-    Parse_Options m_Opts;
-    int m_maxTimeToParseInNumberOfSeconds;
-    int m_disjunctCount;
-    QString m_languagePreference;
-    QString m_currentDictionaryLanguage;
-    QMap<QString, QString> m_listOfAvailableLanguages;
-};
-
+    
+	class LinkGrammarWrapper
+	{
+	public:
+		LinkGrammarWrapper();
+		LinkGrammarWrapper(Dictionary dict, Parse_Options opts);
+		~LinkGrammarWrapper();
+		bool parseSentence(const QString text) const;
+		void cleanUp();
+		bool isLanguageAvailable(QString language);
+		int getMaxTimeToParseInNumberOfSeconds();
+		void setMaxTimeToParseInNumberOfSeconds(int numberOfSeconds);
+		int getDisjunctCount;
+		void setDisjunctCount(int count);
+		QString getLanguage();
+		void setLanguage(QString language);
+		void addLanguageToSetOfSupportedLanguages(QString language, QString dictionaryPath = QString());
+		
+	private:
+		void cleanUpDictionary();
+		void cleanUpParseOptions();
+		Dictionary m_Dict;
+		Parse_Options m_Opts;
+		int m_maxTimeToParseInNumberOfSeconds;
+		int m_disjunctCount;
+		QString m_languagePreference;
+		QString m_currentDictionaryLanguage;
+		QMap<QString, QString> m_listOfAvailableLanguages;
+	};
+	
 #ifdef __cplusplus
 }
 #endif
