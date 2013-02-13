c0e0db7d55b023e3b0ec95471069f7d70b66da40
Garima Joshi <garimajoshi3@yahoo.in>
2013-02-13 00:02:32 -0800

    Update LinkGrammarWrapper.cpp

--- LinkGrammarWrapper.cpp
+++ LinkGrammarWrapper.cpp
@@ -8,129 +8,129 @@
 LinkGrammarWrapper::LinkGrammarWrapper()
 {
     //add the default language to list of supported languages
-    addLanguageToSetOfSupportedLanguagesWithDictionaryPath("en");
-    m_Opts = parse_options_create();
-    m_Dict = dictionary_create_lang("en");
-    m_languagePreference = "en";
-    // default max parsing time = 1
-    m_maxTimeToParseInNumberOfSeconds = 1;	
-    // default disjunct count = 2
-    m_getDisjunctCount = 2;	
+	addLanguageToSetOfSupportedLanguagesWithDictionaryPath("en");
+	m_Opts = parse_options_create();
+	m_Dict = dictionary_create_lang("en");
+	m_languagePreference = "en";
+	// default max parsing time = 1
+	m_maxTimeToParseInNumberOfSeconds = 1;
+	// default disjunct count = 2
+	m_getDisjunctCount = 2;
 }
 
 LinkGrammarWrapper::LinkGrammarWrapper(Dictionary dict, Parse_Options opts)
 {
-    m_Opts = opts;
-    m_Dict = dict;
-    // default max parsing time = 1
-    m_maxTimeToParseInNumberOfSeconds = 1;	
-    // default disjunct count = 2
-    m_getDisjunctCount = 2;	
+	m_Opts = opts;
+	m_Dict = dict;
+	// default max parsing time = 1
+	m_maxTimeToParseInNumberOfSeconds = 1;
+	// default disjunct count = 2
+	m_getDisjunctCount = 2;
 }
 
 LinkGrammarWrapper::~LinkGrammarWrapper()
 {
-    cleanUp();
-    m_listOfAvailableLanguages.clear();
+	cleanUp();
+	m_listOfAvailableLanguages.clear();
 }
 
 void LinkGrammarWrapper::cleanUpDictionary()
 {
-    if(m_Dict)
-        dictionary_delete(m_Dict);
-    m_Dict = 0;
+	if(m_Dict)
+		dictionary_delete(m_Dict);
+	m_Dict = 0;
 }
 
 void LinkGrammarWrapper::cleanUpParseOptions()
 {
-    if(m_Opts)
-        parse_options_delete(m_Opts);
-    m_Opts = 0;
+	if(m_Opts)
+		parse_options_delete(m_Opts);
+	m_Opts = 0;
 }
 
 void LinkGrammarWrapper::cleanUp()
 {
-    cleanUpParseOptions();
-    cleanUpDictionary();
+	cleanUpParseOptions();
+	cleanUpDictionary();
 }
 
 int LinkGrammarWrapper::getMaxTimeToParseInNumberOfSeconds()
 {
-    return m_maxTimeToParseInNumberOfSeconds;
+	return m_maxTimeToParseInNumberOfSeconds;
 }
 
 void LinkGrammarWrapper::setMaxTimeToParseInNumberOfSeconds(int numberOfSeconds)
 {
-    m_maxTimeToParseInNumberOfSeconds = numberOfSeconds;
+	m_maxTimeToParseInNumberOfSeconds = numberOfSeconds;
 }
 
 int LinkGrammarWrapper::getDisjunctCount()
 {
-    return m_disjunctCount;
+	return m_disjunctCount;
 }
 
 void LinkGrammarWrapper::setDisjunctCount(int count)
 {
-    m_disjunctCount = count;
+	m_disjunctCount = count;
 }
 
 QString LinkGrammarWrapper::getLanguage()
 {
-    return m_languagePreference;
+	return m_languagePreference;
 }
 
 bool LinkGrammarWrapper::setLanguage(QString language)
 {
-    m_languagePreference = language;
+	m_languagePreference = language;
 }
 
 void LinkGrammarWrapper::addLanguageToSetOfSupportedLanguages(QString language, QString dictionaryPath = QString())
 {
-    m_listOfAvailableLanguages[languages] = dictionaryPath;
+	m_listOfAvailableLanguages[languages] = dictionaryPath;
 }
 
 bool LinkGrammarWrapper::isLanguageAvailable(QString language)
 {
-    return m_listOfAvailableLanguages.contains(language);
+	return m_listOfAvailableLanguages.contains(language);
 }
 
 bool LinkGrammarWrapper::parseSentence(QString givenSentence)
 {
-    if(!m_Dict)
-    {
-        kDebug() << "No dictionary";
-        return true; //no grammar checking
-    }
-    char *textChar = givenSentence.toUtf8().data();
-    Sentence sent = sentence_create(textChar, m_Dict);
-    if (!sent)
-    {
-        kDebug() << "Failed to create sentence";
-        return true;
-    }
-
-    parse_options_set_max_parse_time(m_Opts, maxTimeToParseInNumberOfSeconds);
-    parse_options_set_disjunct_cost(m_Opts, disjunctCount)
-    
-    parse_options_set_min_null_count(m_Opts, 0);
-    parse_options_set_max_null_count(m_Opts, 0);
-    parse_options_set_islands_ok(m_Opts, 0);
-    parse_options_set_panic_mode(m_Opts, true);
-    parse_options_reset_resources(m_Opts);
-    
-    quint32 num_linkages = sentence_parse(sent, m_Opts);
-    bool res =  (num_linkages >= 1);
-    
-    if(parse_options_timer_expired(m_Opts))
-    {
-        kDebug() << "Timer expired!";
-        res = true;
-    }
-
-    /*****
-    * TODO: find out what actually went wrong by allowing null linkages.
-    *******/
-
-    sentence_delete(sent);
-    return res;
+	if(!m_Dict)
+	{
+		kDebug() << "No dictionary";
+		return true; //no grammar checking
+	}
+	char *textChar = givenSentence.toUtf8().data();
+	Sentence sent = sentence_create(textChar, m_Dict);
+	if (!sent)
+	{
+		kDebug() << "Failed to create sentence";
+		return true;
+	}
+	
+	parse_options_set_max_parse_time(m_Opts, maxTimeToParseInNumberOfSeconds);
+	parse_options_set_disjunct_cost(m_Opts, disjunctCount)
+	
+	parse_options_set_min_null_count(m_Opts, 0);
+	parse_options_set_max_null_count(m_Opts, 0);
+	parse_options_set_islands_ok(m_Opts, 0);
+	parse_options_set_panic_mode(m_Opts, true);
+	parse_options_reset_resources(m_Opts);
+	
+	quint32 num_linkages = sentence_parse(sent, m_Opts);
+	bool res =  (num_linkages >= 1);
+	
+	if(parse_options_timer_expired(m_Opts))
+	{
+		kDebug() << "Timer expired!";
+		res = true;
+	}
+	
+	/*****
+	 * TODO: find out what actually went wrong by allowing null linkages.
+	 *******/
+	
+	sentence_delete(sent);
+	return res;
 }
