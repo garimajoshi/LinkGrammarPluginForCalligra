7b257d5335ca6cc211d77d6ee60ba29c360a9a1f
Garima Joshi <garimajoshi3@yahoo.in>
2013-03-12 00:59:55 +0530

    Update GrammarCheckPlugin.cpp

--- GrammarCheckPlugin.cpp
+++ GrammarCheckPlugin.cpp
@@ -1,6 +1,7 @@
 #include "GrammarCheckPlugin.h"
 #include "GrammarCheckFactory.h"
 
+#include <KDebug>
 #include <KPluginFactory>
 #include <KPluginLoader>
 
@@ -12,6 +13,7 @@ K_EXPORT_PLUGIN(GrammarCheckPluginFactory("GrammarCheckPlugin"))
 GrammarCheckPlugin::GrammarCheckPlugin(QObject *parent, const QVariantList&)
     : QObject(parent)
 {
+    kDebug(31000) << "grammar check instance created";
     KoTextEditingRegistry::instance()->add(new GrammarCheckFactory());
 }
 
